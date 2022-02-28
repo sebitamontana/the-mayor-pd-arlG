47a1a5313e1c96d2007bec56807683aedec36d15
sebitamontana <93792473+sebitamontana@users.noreply.github.com>
2022-02-28 20:10:48 -0300

    Added mainmenu

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -58,7 +58,7 @@ class MainMenuState extends MusicBeatState
 		persistentUpdate = persistentDraw = true;
 
 		var yScroll:Float = Math.max(0.25 - (0.05 * (optionShit.length - 4)), 0.1);
-		var bg:FlxSprite = new FlxSprite(-80).loadGraphic(Paths.image('menuBG'));
+		var bg:FlxSprite = new FlxSprite(-80).loadGraphic(Paths.image('menuDesat'));
 		bg.scrollFactor.set(0, yScroll);
 		bg.setGraphicSize(Std.int(bg.width * 1.175));
 		bg.updateHitbox();
